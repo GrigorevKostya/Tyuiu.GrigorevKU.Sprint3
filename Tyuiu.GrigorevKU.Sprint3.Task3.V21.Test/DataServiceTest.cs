cb8c19f34d1b123b1e8f0be138eaa8b060e955d2
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-10-17 17:14:47 +0500

    Task3

---
+++ Tyuiu.GrigorevKU.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GrigorevKU.Sprint3.Task3.V21.Lib;
+namespace Tyuiu.GrigorevKU.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceNumOnChar()
+        {
+            DataService ds = new DataService();
+            string value = "f3g5ht g4j 34kg4";
+            char item = 'e';
+
+            string res = ds.ReplaceNumOnChar(value, item);
+
+            string wait = "fegeht gej eekge";
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.GrigorevKU.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GrigorevKU.Sprint3.Task3.V21.Lib;
+namespace Tyuiu.GrigorevKU.Sprint3.Task3.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3| Выполнил: Григорьев К. Ю. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил Григорьев К. Ю. | ИИПб-23-2                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая заменяет числа в строке на символ е         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "f3g5ht g4j 34kg4";
+            char item = 'e';
+            Console.WriteLine("Исходная строка: " + value);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Конечная строка: " + ds.ReplaceNumOnChar(value, item));
+            Console.ReadKey();
+        }
+    }
+}
