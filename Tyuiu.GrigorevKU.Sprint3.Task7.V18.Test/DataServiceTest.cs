b41c2f46aa4f92584cb7d4cc38b1183e40942626
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-10-27 20:59:25 +0500

    Task7

---
+++ Tyuiu.GrigorevKU.Sprint3.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GrigorevKU.Sprint3.Task7.V18.Lib;
+namespace Tyuiu.GrigorevKU.Sprint3.Task7.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -45.83;
+            valueWaitArray[1] = 17.83;
+            valueWaitArray[2] = 11.09;
+            valueWaitArray[3] = 19.82;
+            valueWaitArray[4] = -5.25;
+            valueWaitArray[5] = 2;
+            valueWaitArray[6] = 4.15;
+            valueWaitArray[7] = 4.60;
+            valueWaitArray[8] = 4.97;
+            valueWaitArray[9] = 1.35;
+            valueWaitArray[10] = 34.68;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.GrigorevKU.Sprint3.Task7.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint3.Task7.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint3.Task7.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64ffe23a-0abc-4bd5-a33d-277ae859954f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
